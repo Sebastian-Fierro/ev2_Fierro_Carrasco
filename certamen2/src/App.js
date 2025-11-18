f04292378cec0d35599ed0475b3f2179f3e69043
Sebastian-Fierro <sebastian.fierro2101@alumnos.ubiobio.cl>
2025-11-18 14:36:56 +0000

    primer commit

---
+++ certamen2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Regalos from "./components/Regalos";
+import Comida from "./components/Comida";
+import Adornos from "./components/Adornos";
+
+
+export function App() {
+  return (
+    <Fragment>
+      <div style={{ padding: 20 }}>
+        
+      </div>
+    </Fragment>
+  );
+}
