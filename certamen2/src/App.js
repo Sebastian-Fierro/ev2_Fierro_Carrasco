e50a601a58489d551d249ac71a8efe129d800d9f
Sebastian-Fierro <sebastian.fierro2101@alumnos.ubiobio.cl>
2025-11-18 16:21:18 +0000

    cambios menores

--- certamen2/src/App.js
+++ certamen2/src/App.js
@@ -1,7 +1,4 @@
 import React, { Fragment } from "react";
-import Regalos from "./components/Regalos";
-import Comida from "./components/Comida";
-import Adornos from "./components/Adornos";
 
 
 export function App() {
